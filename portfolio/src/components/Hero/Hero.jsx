c3c0a8bb8ad958b0842abfa22977252eafa18b4b
jeff <jeffdelmotte@gmail.com>
2025-03-10 10:44:57 +0100

    intégration de la section "hero" avec ses animations

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -3,12 +3,25 @@ import "./hero.css";
 function Hero() {
   return (
     <section id="hero">
-      <h1 className="hero-title">
-        Delmotte
-        <br />
-        Jean-François
-      </h1>
-      <h2 className="hero-subtitle">Développeur web/intégrateur web</h2>
+      <div className="hero-content">
+        <h1 className="hero-title">
+          Delmotte
+          <br />
+          Jean-François
+        </h1>
+        <h2 className="hero-subtitle">Développeur web/intégrateur web</h2>
+      </div>
+      <div className="hero-buttons">
+        <a
+          href="https://github.com/jeff397?tab=repositories"
+          className="github-button"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <button className="contact-button">Contactez moi</button>
+      </div>
     </section>
   );
 }
