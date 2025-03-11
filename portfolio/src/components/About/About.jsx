21c538cff97f2872e1cc4b0364b8d134770712ba
jeff <jeffdelmotte@gmail.com>
2025-03-11 16:33:59 +0100

    correction

---
+++ portfolio/src/components/About/About.jsx
@@ -0,0 +1,21 @@
+import "./about.css";
+
+function About() {
+  return (
+    <section id="about">
+      <div className="about-content">
+        <h3>A propos de moi</h3>
+        <p>
+          Je suis un développeur web autodidacte avec une forte passion pour la
+          création de sites web. J'ai développé plusieurs projets personnels en
+          utilisant des technologies telles que HTML, CSS, JavaScript et React.
+          Je suis motivé par les défis techniques et j'aime travailler sur des
+          projets qui améliorent l'expérience utilisateur tout en étant
+          visuellement attractifs.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
