d21beef9f81d9b89ff455ce5a5dfa6599bb562c8
jeff <jeffdelmotte@gmail.com>
2025-03-14 09:37:41 +0100

    intégration d'un background et du lien vers l'accueil

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import "./header.css";
-import logo from "../../../public/images/Logo.png";
+import logo from "../../../public/images/github.png";
 
 function Header() {
   return (
     <header>
-      <a href="./">
+      <a href="https://github.com/jeff397?tab=repositories">
         <img src={logo} alt="Logo" className="logo" />
       </a>
       <nav className="nav-links">
+        <a href="#hero">Accueil</a>
         <a href="#about">Présentation</a>
         <a href="#projects">Projets</a>
         <a href="/CV.pdf" download>
