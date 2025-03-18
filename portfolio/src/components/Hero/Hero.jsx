2410f49548331279a0c112e400d1486157f3ae32
jeff <jeffdelmotte@gmail.com>
2025-03-18 16:11:48 +0100

    mise en place des médias quieries

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -4,26 +4,28 @@ import "./../../Styles/keyframes.css";
 function Hero() {
   return (
     <section id="hero">
-      <div className="hero-content">
-        <h1 className="hero-title">
-          Delmotte
-          <br />
-          Jean-François
-        </h1>
-        <h2 className="hero-subtitle">Développeur web/intégrateur web</h2>
-      </div>
-      <div className="hero-buttons">
-        <a
-          href="https://github.com/jeff397?tab=repositories"
-          className="github-button"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-        <a href="#contact">
-          <button className="contact-button">Contactez moi</button>
-        </a>
+      <div className="hero-background">
+        <div className="hero-content">
+          <h1 className="hero-title">
+            Delmotte
+            <br />
+            Jean-François
+          </h1>
+          <h2 className="hero-subtitle">Développeur web/intégrateur web</h2>
+        </div>
+        <div className="hero-buttons">
+          <a
+            href="https://github.com/jeff397?tab=repositories"
+            className="github-button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <a href="#contact">
+            <button className="contact-button">Contactez moi</button>
+          </a>
+        </div>
       </div>
     </section>
   );
