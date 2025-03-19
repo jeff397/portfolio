3e0b865b11f346923212dc51bd73ee27be709cfc
jeff <jeffdelmotte@gmail.com>
2025-03-19 11:49:08 +0100

    intégration de react router dom

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
@@ -5,21 +6,30 @@ import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import CV from "./pages/cv";
 import "./index.css";
 
 function App() {
   return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Header />
+            <main>
+              <Hero />
+              <About />
+              <Skills />
+              <Projects />
+              <Contact />
+            </main>
+            <Footer />
+          </>
+        }
+      />
+      <Route path="/cv" element={<CV />} />
+    </Routes>
   );
 }
 
